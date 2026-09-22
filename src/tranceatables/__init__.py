55de1c451b130aca7ec02710f9857591c7f7c777
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2026-09-22 12:50:26 -0500

    Activate TRANCEatables Stage 1 API

--- src/tranceatables/__init__.py
+++ src/tranceatables/__init__.py
@@ -1,12 +1,31 @@
-"""TRANCEatables Stage 0 domain core."""
+"""TRANCEatables Stage 1 domain core."""
 
 from .feasibility import FeasibilityDecision, evaluate_delivery
 from .models import DeliveryRequest, FoodHandling, RobotState
+from .order_state import InvalidTransition, Order, OrderStatus, can_transition
+from .repository import (
+    ConcurrentUpdateError,
+    DuplicateOrderError,
+    OrderEvent,
+    OrderNotFoundError,
+    SQLiteOrderRepository,
+)
+from .service import OrderService
 
 __all__ = [
+    "ConcurrentUpdateError",
     "DeliveryRequest",
+    "DuplicateOrderError",
     "FeasibilityDecision",
     "FoodHandling",
+    "InvalidTransition",
+    "Order",
+    "OrderEvent",
+    "OrderNotFoundError",
+    "OrderService",
+    "OrderStatus",
     "RobotState",
+    "SQLiteOrderRepository",
+    "can_transition",
     "evaluate_delivery",
 ]
